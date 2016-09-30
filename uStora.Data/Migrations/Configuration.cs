d28b89cf82d8239920abd327a54a387eb33ccc81
dvbtham <dvbtham@gmail.com>
2016-09-30 22:43:28 +0700

    Hoàn thành tầng uStora.UnitTest

--- uStora.Data/Migrations/Configuration.cs
+++ uStora.Data/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
 namespace uStora.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Model.Models;
+    using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<uStoraDbContext>
     {
@@ -11,18 +16,40 @@ namespace uStora.Data.Migrations
 
         protected override void Seed(uStoraDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            CreateUser(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void CreateUser(uStoraDbContext context)
+        {
+            if (context.Users.Count() == 0)
+            {
+                var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new uStoraDbContext()));
+
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new uStoraDbContext()));
+
+                var user = new ApplicationUser()
+                {
+                    UserName = "dvbtham",
+                    Email = "dvbtham@gmail.com",
+                    EmailConfirmed = true,
+                    BirthDay = DateTime.Now,
+                    FullName = "Thâm David",
+                    Address = "Gia Lai",
+                    PhoneNumber = "01652130546"
+                };
+
+                manager.Create(user, "123123$");
+
+                if (!roleManager.Roles.Any())
+                {
+                    roleManager.Create(new IdentityRole { Name = "Admin" });
+                    roleManager.Create(new IdentityRole { Name = "User" });
+                }
+
+                var adminUser = manager.FindByEmail("dvbtham@gmail.com");
+
+                manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            }
         }
     }
 }
