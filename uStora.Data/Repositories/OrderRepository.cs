10a71939c49d1951f89efda708436e6c9ff0e3de
dvbtham <dvbtham@gmail.com>
2016-09-30 22:10:38 +0700

    Hoàn thành tầng uStora.Data + uStora.Model

---
+++ uStora.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using uStora.Data.Infrastructure;
+using uStora.Model.Models;
+
+namespace uStora.Data.Repositories
+{
+    public interface IOrderRepository : IRepository<Order> { }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ uStora.Data/uStoraDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using uStora.Model.Models;
+using System.Data.Entity;
+
+namespace uStora.Data
+{
+    public class uStoraDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public uStoraDbContext()
+            : base("uStoraConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Error> Errors { get; set; }
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<SupportOnline> SupportOnlines { get; set; }
+        public DbSet<SystemConfig> SystemConfigs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ContactDetail> ContactDetails { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+
+        public static uStoraDbContext Create()
+        {
+            return new uStoraDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId });
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(i => i.UserId);
+        }
+    }
+}
