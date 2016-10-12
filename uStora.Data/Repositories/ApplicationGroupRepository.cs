f8876e89555ed790567813267c6762fb88cae12a
dvbtham <dvbtham@gmail.com>
2016-10-12 11:47:54 +0700

    thêm một số repository cho việc quản lý người dùng

---
+++ uStora.Data/Repositories/ApplicationGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using uStora.Data.Infrastructure;
+using uStora.Model.Models;
+
+namespace uStora.Data.Repositories
+{
+    public interface IApplicationGroupRepository : IRepository<ApplicationGroup>
+    {
+    }
+
+    public class ApplicationGroupRepository : RepositoryBase<ApplicationGroup>, IApplicationGroupRepository
+    {
+        public ApplicationGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ uStora.Data/Repositories/ApplicationRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using uStora.Data.Infrastructure;
+using uStora.Model.Models;
+
+namespace uStora.Data.Repositories
+{
+    public interface IApplicationRoleRepository : IRepository<ApplicationRole>
+    {
+    }
+
+    public class ApplicationRoleRepository : RepositoryBase<ApplicationRole>, IApplicationRoleRepository
+    {
+        public ApplicationRoleRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
